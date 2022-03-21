1dcfb2cc6ba3aa443160bcb3482e90d81c2f1e1b
sabina489 <Karkisabina869@gmail.com>
2022-03-21 15:45:32 +0545

    learn rest

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from snippets import views
 
+#tfrttyy
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
